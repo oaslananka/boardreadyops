884f5fa31f8fd701693c533747c69eb7d13f5464
Osman Aslan <oaslananka@gmail.com>
2026-08-05 03:18:03 +0300

    chore(main): release 1.30.1 (#322)

--- dist/cli/index.cjs
+++ dist/cli/index.cjs
@@ -21885,7 +21885,7 @@ function useColor() {
 var program = new Command();
 
 // src/generated/version.ts
-var boardReadyVersion = "1.30.0";
+var boardReadyVersion = "1.30.1";
 
 // src/i18n/en.ts
 var en = {
