e91dca5664e9ddf763e923c9cca99f2c375650da
Osman Aslan <info@oaslananka.dev>
2026-09-14 20:59:20 +0300

    chore(main): release 1.44.0 (#737)
    Regenerated release and compliance artifacts.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- dist/cli/index.cjs
+++ dist/cli/index.cjs
@@ -48,7 +48,7 @@ var boardReadyVersion;
 var init_version = __esm({
   "src/generated/version.ts"() {
     "use strict";
-    boardReadyVersion = "1.43.1";
+    boardReadyVersion = "1.44.0";
   }
 });
 
--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,1 +1,1 @@
-export const boardReadyVersion = "1.43.1"; // x-release-please-version
+export const boardReadyVersion = "1.44.0"; // x-release-please-version
