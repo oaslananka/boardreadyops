16642909836fe23fa674e40a3fa6acb469adef81
Osman Aslan <oaslananka@gmail.com>
2026-09-18 21:33:54 +0300

    chore(main): release 1.66.0
    Regenerated release and compliance artifacts.

--- dist/cli/index.cjs
+++ dist/cli/index.cjs
@@ -48,7 +48,7 @@ var boardReadyVersion;
 var init_version = __esm({
   "src/generated/version.ts"() {
     "use strict";
-    boardReadyVersion = "1.65.1";
+    boardReadyVersion = "1.66.0";
   }
 });
 
